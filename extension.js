d0b1fc95d29452088d50d1a0ad399cafea42636e
Qian Xiao <heyheyco@gmail.com>
2018-06-02 04:00:05 -0700

    Initial commit: Add scroll-to-switch-workspace functionality

---
+++ extension.js
@@ -0,0 +1,74 @@
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Meta = imports.gi.Meta;
+const Clutter = imports.gi.Clutter;
+const Layout = imports.ui.layout;
+
+let button, actor, wrapRight;
+
+function _onScroll(actor, event) {
+    let motion;
+    let dir = event.get_scroll_direction()
+    switch(dir) {
+        case Clutter.ScrollDirection.UP:
+            motion = Meta.MotionDirection.UP;
+            break;
+        case Clutter.ScrollDirection.DOWN:
+            motion = Meta.MotionDirection.DOWN;
+            break;
+        case Clutter.ScrollDirection.LEFT:
+            motion = Meta.MotionDirection.LEFT;
+            break;
+        case Clutter.ScrollDirection.RIGHT:
+            motion = Meta.MotionDirection.RIGHT;
+            break;
+        default:
+            return Clutter.EVENT_PROPAGATE;
+    }
+    let activeWs = global.screen.get_active_workspace();
+    let ws = activeWs.get_neighbor(motion);
+    if(!ws) return Clutter.EVENT_STOP;
+    Main.wm.actionMoveWorkspace(ws);
+    return Clutter.EVENT_STOP;
+}
+
+function init() {
+
+}
+
+function enable() {
+    //add functioning button
+    button = new St.Bin({ style_class: '',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true });
+    let monitor = Main.layoutManager.primaryMonitor;
+    button.set_size(1, monitor.height);
+    button.set_position(monitor.width - 1, 0);
+    Main.uiGroup.add_actor(button);
+    button.connect("scroll-event", _onScroll);
+
+    //add wrap of the button to make the button react to event even when other windows are on top of it
+    actor = new St.Widget({
+        clip_to_allocation: true,
+        layout_manager: new Clutter.BinLayout()
+    });
+    actor.add_constraint(new Layout.MonitorConstraint({primary: true, work_area: true}));
+    Main.layoutManager.addChrome(actor, {affectsInputRegion: false});
+    wrapRight = new St.Widget({x_expand: true, y_expand: true,
+        x_align: Clutter.ActorAlign.END,
+        y_align: Clutter.ActorAlign.END});
+    wrapRight.set_size(5, monitor.height);
+
+    //Deploy wrappers
+    actor.add_actor(wrapRight);
+    Main.layoutManager.trackChrome(wrapRight, {affectsInputRegion: true});
+}
+
+function disable() {
+    Main.layoutManager.removeChrome(actor);
+    Main.layoutManager.untrackChrome(wrapRight);
+    button.destroy();
+}
